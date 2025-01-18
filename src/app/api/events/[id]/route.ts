21c531eb39e55134ee4f727a6414ca0331697417
Fitrahtur Rahman <fitrahtur@gmail.com>
2025-01-18 17:46:27 +0700

    fix params type

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -1,10 +1,7 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, params: { id: string }) {
   const id = params.id
 
   const supabase = await createClient()
