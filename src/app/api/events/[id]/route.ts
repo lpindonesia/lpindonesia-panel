94b25835d217bc71304f19e5ff2a2c47f5017e08
Fitrahtur Rahman <fitrahtur@gmail.com>
2025-01-18 15:35:08 +0700

    wiring update participants function

---
+++ src/app/api/events/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id
+
+  const supabase = await createClient()
+  const { data: event } = await supabase
+    .from('events')
+    .select('*')
+    .eq('id', parseInt(id))
+    .single()
+  return NextResponse.json({ error: null, data: event })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,55 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
 const FIELD_NAMES = {
   CURRENT_PARTICIPANTS: 'current_participants',
   MAX_PARTICIPANTS: 'max_participants',
 }
 
-export default function Home() {
-  async function updateEvent(formData: FormData) {
+export default async function Home() {
+  const supabase = await createClient()
+  const { data: event } = await supabase
+    .from('events')
+    .select('*')
+    .eq('id', 1)
+    .single()
+
+  async function updateEventParticipants(formData: FormData) {
     'use server'
+    const supabase = await createClient()
 
     const rawFormData = {
-      max_participants: Number(formData.get(FIELD_NAMES.CURRENT_PARTICIPANTS)),
-      current_participants: Number(formData.get(FIELD_NAMES.MAX_PARTICIPANTS)),
-      id: 1,
+      current_participants: Number(
+        formData.get(FIELD_NAMES.CURRENT_PARTICIPANTS)
+      ),
+      max_participants: Number(formData.get(FIELD_NAMES.MAX_PARTICIPANTS)),
     }
+
+    await supabase.from('events').update(rawFormData).eq('id', 1)
+
+    redirect('/')
   }
 
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <form action={updateEvent} method='patch'>
+      <form action={updateEventParticipants}>
+        <label htmlFor={FIELD_NAMES.CURRENT_PARTICIPANTS}>
+          Current participants
+        </label>
         <input
           name={FIELD_NAMES.CURRENT_PARTICIPANTS}
           type='number'
           placeholder='Number of current participants'
+          defaultValue={event?.current_participants ?? 0}
         />
+        <label htmlFor={FIELD_NAMES.MAX_PARTICIPANTS}>
+          Maximum participants
+        </label>
         <input
           name={FIELD_NAMES.MAX_PARTICIPANTS}
           type='number'
           placeholder='Maximum number of participants'
+          defaultValue={event?.max_participants ?? 0}
         />
         <button type='submit'>Update</button>
       </form>
